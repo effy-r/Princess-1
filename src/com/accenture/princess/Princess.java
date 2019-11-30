df0d2c464634c166a5c82c8a2d658840c751267d
Sandra Smirnova <sandra.smirnova@ymail.com>
2019-11-30 15:34:15 +0200

    Part 1

---
+++ src/com/accenture/princess/Princess.java
@@ -0,0 +1,59 @@
+package com.accenture.princess;
+
+public class Princess {
+
+	private String princessName;
+	private String castle;
+	private int gold;
+	
+	public Princess() {
+
+	}
+
+	public Princess(String princessName, String castle, int gold) {
+
+	}
+
+	public String getPrincessName() {
+		return princessName;
+	}
+
+	public String getCastle() {
+		return castle;
+	}
+
+	public int getGold() {
+		return gold;
+	}
+
+	public boolean hasGold() {
+
+		if (gold > 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	public void giveGold () {
+		this.gold = gold + 10;
+	}
+	
+	public void takeGold () {
+		this.gold = gold - 10;
+		
+	}
+	
+	public void changeCastleName (String castle) {		
+		this.castle = castle;
+		
+	}
+	
+	public void losesCastle (){
+		if (!hasGold()){
+			System.out.println (" The Princess " + princessName + " loses her Castle, the Castle " + castle + " is available for anybody!");
+	}
+	}
+
+}
+
+
